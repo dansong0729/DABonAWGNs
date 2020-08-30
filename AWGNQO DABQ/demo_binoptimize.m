a5e54e9325ae6245967fdd61abb349ba15173fa4
dansong0729 <dansong0729@gmail.com>
2020-08-30 18:24:12 -0400

    Add optimizebins symmetry speedup
    Add option to compute only negative half of the boundaries when problem is symmetric about 0. Add and modify corresponding test code.

--- AWGNQO DABQ/demo_binoptimize.m
+++ AWGNQO DABQ/demo_binoptimize.m
@@ -3,7 +3,7 @@
 xsupport = [-1; 1];
 pX = [.5; .5];
 N = rand(1)*5;
-optimizebins(pX, xsupport, N)
+optimizebins(pX, xsupport, N, true)
 %% fuzzing, trying to fix negative probability
 for i = 1:200
     n = randi(5, 1)+1;
@@ -12,6 +12,23 @@ for i = 1:200
     pX = pX./sum(pX);
     N = (rand(1)+.1)*2;
 
-    optimizebins(pX, xsupport, N);
+    optimizebins(pX, xsupport, N, false);
+end
+"end"
+%% test symmetric
+for i = 1:200
+    n = randi(10, 1)+1;
+    xsupport = sort(rand(floor(n/2),1)*4);
+    if mod(n,2)
+        xsupport = [sort(-xsupport); 0; xsupport];
+    else
+        xsupport = [sort(-xsupport); xsupport];
+    end
+    
+    pX = rand(n, 1);
+    pX = pX./sum(pX);
+    N = (rand(1)+.1)*2;
+    
+    optimizebins(pX, xsupport, N, true);
 end
 "end"
--- AWGNQO DABQ/optimizebins.m
+++ AWGNQO DABQ/optimizebins.m
@@ -1,4 +1,4 @@
-function [q] = optimizebins(pX, xsupport, N)
+function [q] = optimizebins(pX, xsupport, N, isSymmetric)
 %OPTIMIZEBINS Find optimal bins for AWGNQO channel given input pmf
 %   Performs alternating optimization on each bin threshold using fminbnd
 %   and terminates on sufficiently small mutual information gain between
@@ -8,6 +8,7 @@ function [q] = optimizebins(pX, xsupport, N)
 %   pX: input pmf, as column probability vector
 %   xsupport: input pmf support points in ASCENDING order, as column vector
 %   N: channel noise power
+%   isSymmetric: set true to exploit xsupport symmetry about 0
 %   
 %   OUTPUT:
 %   q: vector of optimal bin thresholds in ascending order, as column
@@ -24,22 +25,38 @@ maxIter = 1000;
 q = (xsupport(2:end) + xsupport(1:end-1))./2;
 q = [-Inf; q; Inf];
 
+qsize = size(q,1); %used a lot when isSymmetric
 
-options = optimset('FunValCheck','on'); %for debugging
-% %=====DEBUG=====
-% qhist = [];
-% %=====END=====
+options = optimset('FunValCheck','on'); %for safety
+
 for iter = 1:maxIter
     oldMI = discreteMI(pX, getawgnqtransition(xsupport, q, N));
-    for i = 2:size(q,1)-1
+    indices = 2:qsize-1;
+    %only do half if symmetric
+    if isSymmetric
+        indices = 2:floor(qsize/2);
+        if mod(qsize,2)
+            %middle element is 0 when symmetric and odd
+            q(ceil(qsize/2)) = 0; 
+        end
+    end
+    %indices = indices(randperm(qsize)); %random order
+    for i = indices
         %function to minimize:
         %negative of q(i) conntribution to mutual information
         objective = @(qi) -discreteMI(pX, getawgnqtransition(xsupport, [q(i-1); qi; q(i+1)], N));
         %deal with infinites in bounds
         lower = max(q(i-1), q(i)-abs(q(i))-endgap*10);
         upper = min(q(i+1), q(i)+abs(q(i))+endgap*10);
+        if isSymmetric
+            upper = min(upper, 0); %only do negatives when symmetric
+        end
         %maximize MI over q(i)
         q(i) = fminbnd(objective, lower, upper, options);
+        %assign symmetric point
+        if isSymmetric
+            q(qsize+1-i) = -q(i);
+        end
     end
     newMI = discreteMI(pX, getawgnqtransition(xsupport, q, N));
     %check MI convergence
@@ -47,24 +64,6 @@ for iter = 1:maxIter
         %if no boundary moved more that endgap
         break
     end
-%     %=====DEBUG=====
-%     qhist = [qhist q];
-%     %=====END=====
 end
-% %=====DEBUG=====
-% if iter > 100
-%     iter
-%     pX = pX
-%     xsupport = xsupport
-%     N = N
-%     figure
-%     hold on
-%     for qi = qhist'
-%         plot([1:iter]', qi)
-%     end
-%     plot(zeros(size(xsupport), 1), xsupport)
-% end
-% %=====END=====
-
 end
 
