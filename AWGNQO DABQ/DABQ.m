0bf06965dfa9d92428848a4822ae7c094f05505b
Dan Song <dansong0729@gmail.com>
2022-01-16 14:23:23 -0800

    Add documentation

--- AWGNQO DABQ/DABQ.m
+++ AWGNQO DABQ/DABQ.m
@@ -1,14 +1,27 @@
-function [pX, xsupport, q, MI, s] = DABQ(N, E, supp_init, p_init, q_init)
-%DABQ Summary of this function goes here
-%   Detailed explanation goes here
+function [pX, xsupport, q, MI, s] = DABQ(N, E, supp_init, p_init, q_init, varargin)
+%DABQ Implementation of DAB for quantized AWGN channel
+%   INPUTS:
+%   N: channel noise power
+%   E: input power constraint
+%   supp_init: col to initialize support points
+%   p_init: col to initialize probabilities
+%   q_init: col to initialize bins
+%
+%   OUTPUTS:
+%   pX: col of optimal probabilities
+%   xsupport: col of optimal support points
+%   q: col of optimal bin thresholds, including +- INF endpoints
+%   MI: mutual information achieved
+%   s: langrange multiplier for power constraint
+
 
 %settings
 maxIter = 1e2;
 ETolerance = 1e-5;
 BAETolerance = 1e-5;
 DABTolerance = 3e-5;
-prob_thresh = 0; %1e-10; %0 to disable remove redundant
-sup_thresh = 0; %1e-4; %^ same
+prob_thresh = 1e-6; %0 to disable remove redundant
+sup_thresh = 1e-4; %^ same
 DABiter = 1e2;
 tol = 1e-4;
 
